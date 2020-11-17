9aa016cff5b7afaac61422af2404d5c5a1130178
asiddiki98 <azimsardines@gmail.com>
2020-11-17 15:10:03 -0500

    "finish class inheritance

--- class_inheritance/employee.rb
+++ class_inheritance/employee.rb
@@ -1,11 +1,13 @@
+require "byebug"
 class Employee
     attr_reader :name, :title, :salary, :boss
 
-    def initialize(name, title, salary, boss)
+    def initialize(name, title, salary, boss=nil)
         @name = name
         @title = title
         @salary = salary
         @boss = boss
+        boss.add_employee(self) if !boss.nil? 
     end
 
     def boss=(new_boss)
@@ -21,7 +23,7 @@ end
 class Manager < Employee
     attr_reader :employees
 
-    def initialize(name, title, salary, boss)
+    def initialize(name, title, salary, boss=nil)
         super(name, title, salary, boss)
         @employees = []
     end
@@ -35,16 +37,33 @@ class Manager < Employee
 
     def bonus(multiplier)
         total_employees = []
+        # debugger
         self.employees.each do |employee|
             if employee.is_a?(Manager)
+                # debugger
+                total_employees << employee
                 total_employees << employee.employees
             else
+                # debugger
                 total_employees << employee
             end
         end
+        # debugger
         employed = total_employees.flatten
-        salaries = total_employees.map { |employee| employees.salary}.sum
+        # debugger
+        # employed.each do |employee|
+        #     p employee.name
+        #  end 
+        # debugger
+        salaries = employed.map { |employee| employee.salary }.sum
 
         salaries * multiplier
     end
 end
+
+
+ned = Manager.new("Ned","Founder",1000000)
+darren = Manager.new("Darren", "TA Manager", 78000, ned)
+shawna = Employee.new("Shawna", "TA", 12000, darren)
+david = Employee.new("David", "TA", 10000, darren) 
+p ned.bonus(5)
